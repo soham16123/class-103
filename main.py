bc5aba9c407707ba6f78bde8f19f2ab9c12ca1d8
soham16123 <106761393+soham16123@users.noreply.github.com>
2022-08-21 19:59:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# from_dir = "ENTER THE PATH OF DOWNLOAD FOLDER (USE " / ") in VSC"
+# to_dir = "ENTER THE PATH OF DESTINATION FOLDER(USE " / ") in VSC"
+
+from_dir = "C:/Users/preet/Downloads"
+to_dir = "C:/Users/preet/Desktop/Downloaded_Files"
+
+dir_tree = {
+    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
+    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
+    "Document_Files": ['.ppt', '.xls', '.csv', '.pdf', '.txt'],
+    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
+}
+
+# Event Hanlder Class
+
+class FileMovementHandler(FileSystemEventHandler):
+
+    def on_created(self, event):
+        print(event)
+        print(event.src_path)
+
+
+# Initialize Event Handler Class
+event_handler = FileMovementHandler()
+
+
+# Initialize Observer
+observer = Observer()
+
+# Schedule the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+
+# Start the Observer
+observer.start()
+
+
+while True:
+    time.sleep(2)
+    print("running...")
+
+    
+    print("running...")
+
